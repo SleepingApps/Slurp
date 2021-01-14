33c18c98b250065b084fd7b8ecf2fce679f69a93
raphelkabir <raphelkabir@outlook.com>
2021-01-14 17:14:10 +0000

    #17 Sistemato bug

--- app/src/main/java/it/sofk/slurp/food/weekly/WeeklyFragment.java
+++ app/src/main/java/it/sofk/slurp/food/weekly/WeeklyFragment.java
@@ -22,7 +22,7 @@ public class WeeklyFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_occasionally, container, false);
+        View view = inflater.inflate(R.layout.fragment_weekly, container, false);
 
         holder = new Holder(view);
         return view;
