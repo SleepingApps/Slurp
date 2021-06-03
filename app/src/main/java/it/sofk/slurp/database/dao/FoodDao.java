62e7fa5929fca4817fc384b673259d2edb4b0ce5
LucaF <lucafalasca08@gmail.com>
2021-06-03 19:25:27 +0200

    #56 - MultiRow of FoodInstances

--- app/src/main/java/it/sofk/slurp/database/dao/FoodDao.java
+++ app/src/main/java/it/sofk/slurp/database/dao/FoodDao.java
@@ -36,7 +36,7 @@ public interface FoodDao {
     @Query("SELECT * FROM food_instance")
     LiveData<List<FoodInstance>> getFoods();
 
-    @Query("SELECT food_instance.* FROM food_instance JOIN food_type WHERE frequency = :frequency")
+    @Query("SELECT DISTINCT food_instance.* FROM food_instance JOIN food_type JOIN FoodGroup WHERE frequency = :frequency ORDER BY macroGroup")
     LiveData<List<FoodInstance>> getFoods(Frequency frequency);
     
     @Query("SELECT * FROM food_instance WHERE foodType = :foodType AND date = :date")
