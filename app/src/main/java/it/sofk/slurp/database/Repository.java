123cd8b43191e80002e4456a586271be8388eddf
LucaF <lucafalasca08@gmail.com>
2022-01-08 23:05:25 +0100

    #105 - Clean some useless or deprecated function in ViewModel, Repository and FoodDao

--- app/src/main/java/it/sofk/slurp/database/Repository.java
+++ app/src/main/java/it/sofk/slurp/database/Repository.java
@@ -21,7 +21,6 @@ public class Repository {
 
     private FoodDao foodDao;
     private UserDao userDao;
-    private LiveData<List<FoodInstance>> foodIstances;
     private LiveData<List<FoodType>> foodTypes;
     private LiveData<User> user;
 
@@ -30,7 +29,6 @@ public class Repository {
         Database database = Database.getInstance(application);
         foodDao = database.foodDao();
         userDao = database.userDao();
-        foodIstances = foodDao.getFoods();
         foodTypes = foodDao.getFoodTypes();
         user = userDao.getUser();
     }
@@ -57,10 +55,6 @@ public class Repository {
         Database.databaseWriteExecutor.execute(() -> userDao.update(user));
     }
 
-    public LiveData<List<FoodInstance>> getFoodIstances(){
-        return foodIstances;
-    }
-
     public void insert(FoodInstance foodInstance){
         Database.databaseWriteExecutor.execute(() -> foodDao.insert(foodInstance));
     }
@@ -69,47 +63,12 @@ public class Repository {
         return foodTypes;
     }
 
-    public LiveData<List<FoodType>> getFoodTypes(Frequency frequency) {
-        return foodDao.getFoodTypes(frequency);
-    }
-
-    public LiveData<FoodInstance> getFoodInstance(String foodType, LocalDate date){
-        return foodDao.getFood(foodType, date);
-    }
-
-    public FoodType getFoodType(String name) {
-        return foodDao.getFoodType(name);
-    }
-
     public LiveData<List<FoodInstance>> getFoodIstances(Frequency frequency, LocalDate date){
         return foodDao.getFoods(frequency, date);
     }
 
     public void update(FoodInstance foodInstance){
         Database.databaseWriteExecutor.execute(() -> foodDao.update2(foodInstance));
-    }
-
-    public void updateFromAlternativeName(String name, double number){
-        Database.databaseWriteExecutor.execute(() -> foodDao.updateFromAlternativeName(name, number));
-    }
-
-    public FoodGroup getFoodGroupByName(String name){
-        return foodDao.getFoodGroup(name);
-    }
-
-    public LiveData<List<String>> getEquivalentsNames(Frequency frequency) {
-        return foodDao.getFoodNames(frequency);
-    }
-
-    public Integer maxPortion(String equivalentName, CaloricIntake caloricIntake){
-        Integer max = foodDao.maxPortion(equivalentName, caloricIntake);
-        if(max == null) max = 0;
-
-        return max;
-    }
-
-    public List<FoodInstance> getFoodInstancesFromAlternativeName(String name){
-        return foodDao.getFoodInstancesFromAlternativeName(name);
     }
 
     public LiveData<List<FoodDTO>> getFoods(Frequency frequency, LocalDate date, CaloricIntake caloricIntake){
