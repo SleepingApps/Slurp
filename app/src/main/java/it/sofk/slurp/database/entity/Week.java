66411123f4a45f50074b804977eac88b605f5283
raphelkabir <raphelkabir@outlook.com>
2023-02-17 22:21:14 +0000

    #145 - Add endDate to week

--- app/src/main/java/it/sofk/slurp/database/entity/Week.java
+++ app/src/main/java/it/sofk/slurp/database/entity/Week.java
@@ -12,16 +12,24 @@ public class Week {
     @PrimaryKey
     @NonNull
     public LocalDate startDate;
+    public LocalDate endDate;
 
-    public Week(LocalDate startDate) {
+    public Week(@NonNull LocalDate startDate) {
         this.startDate = startDate;
+        this.endDate = startDate.plusDays(7);
     }
 
-    public LocalDate getStartDate() {
+    public @NonNull LocalDate getStartDate() {
         return startDate;
     }
+    public LocalDate getEndDate() {
+        return endDate;
+    }
 
-    public void setStartDate(LocalDate startDate) {
+    public void setStartDate(@NonNull LocalDate startDate) {
         this.startDate = startDate;
+    }
+    public void setEndDate(@NonNull LocalDate endDate) {
+        this.endDate = endDate;
     }
 }
